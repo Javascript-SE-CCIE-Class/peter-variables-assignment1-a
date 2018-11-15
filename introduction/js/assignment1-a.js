f268027affbc1e5fa11d645f4429d1e93a8cb221
peterwathithi <peterwathithi8@gmail.com>
2018-11-15 17:32:42 +0300

    add introduct

---
+++ introduction/js/assignment1-a.js
@@ -0,0 +1,32 @@
+let height;
+let base;
+let fraction;
+
+height=10;
+base=8;
+fraction=0.5;
+
+let area;
+area=height*base*fraction;
+console.log("My area of the triangle is ",area);
+
+let division;
+let num1=1000;
+let num2=250;
+division=num1/num2;
+console.log("The division of the two numbers is ",division);
+
+let name;
+let dateOfbirth;
+let currentyear;
+let age;
+
+name="John Doe";
+dateOfbirth=1980;
+currentyear=2018;
+age=currentyear-dateOfbirth;
+
+
+console.log("John Doe was born in ",dateOfbirth,"he is",age,"years");
+
+
---
+++ introduction/js/introduction.js
@@ -0,0 +1,16 @@
+console.log('here it is test');
+var student;
+student="Clark Kent"
+console.log(student);
+
+var num1=2500;
+var num2=50;
+
+var sum=num1+num2;
+var  mult=num1*num2;
+var dev=num1/num2;
+
+console.log("My numbers are:", num1, num2);
+console.log("sum is:",sum);
+console.log("multiplication is:",mult);
+console.log("Division is",dev);
